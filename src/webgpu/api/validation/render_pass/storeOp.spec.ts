b4fd48532db8429352458ef7983390cd33e6d95d
Kai Ninomiya <kainino@chromium.org>
2020-07-16 14:13:51 -0700

    suggestions

--- src/webgpu/api/validation/render_pass/storeOp.spec.ts
+++ src/webgpu/api/validation/render_pass/storeOp.spec.ts
@@ -8,149 +8,55 @@ import { ValidationTest } from './../validation_test.js';
 
 export const g = makeTestGroup(ValidationTest);
 
-g.test('depthStoreOp_must_be_store_when_depthReadOnly_is_true').fn(async t => {
-  const depthAttachment = t.device.createTexture({
-    format: 'depth24plus-stencil8',
-    size: { width: 1, height: 1, depth: 1 },
-    usage: GPUTextureUsage.COPY_SRC | GPUTextureUsage.OUTPUT_ATTACHMENT,
+g.test('store_op_and_read_only')
+  .params([
+    { readonly: true, _valid: true },
+    // Using depthReadOnly=true and depthStoreOp='clear' should cause a validation error.
+    { readonly: true, depthStoreOp: 'clear', _valid: false },
+    // Using stencilReadOnly=true and stencilStoreOp='clear' should cause a validation error.
+    { readonly: true, stencilStoreOp: 'clear', _valid: false },
+    // Mismatched depthReadOnly and stencilReadOnly values should cause a validation error.
+    { readonly: false, _valid: true },
+    { readonly: false, depthReadOnly: true, _valid: false },
+    { readonly: false, stencilReadOnly: true, _valid: false },
+    // depthReadOnly and stencilReadOnly should default to false.
+    { readonly: undefined, _valid: true },
+    { readonly: undefined, depthReadOnly: true, _valid: false },
+    { readonly: undefined, stencilReadOnly: true, _valid: false },
+  ] as const)
+  .fn(async t => {
+    const {
+      readonly,
+      depthStoreOp = 'store',
+      depthReadOnly = readonly,
+      stencilStoreOp = 'store',
+      stencilReadOnly = readonly,
+      _valid,
+    } = t.params;
+
+    const depthAttachment = t.device.createTexture({
+      format: 'depth24plus-stencil8',
+      size: { width: 1, height: 1, depth: 1 },
+      usage: GPUTextureUsage.OUTPUT_ATTACHMENT,
+    });
+    const depthAttachmentView = depthAttachment.createView();
+
+    const encoder = t.device.createCommandEncoder();
+    const pass = encoder.beginRenderPass({
+      colorAttachments: [],
+      depthStencilAttachment: {
+        attachment: depthAttachmentView,
+        depthLoadValue: 'load',
+        depthStoreOp,
+        depthReadOnly,
+        stencilLoadValue: 'load',
+        stencilStoreOp,
+        stencilReadOnly,
+      },
+    });
+    pass.endPass();
+
+    t.expectValidationError(() => {
+      encoder.finish();
+    }, !_valid);
   });
-  const depthAttachmentView = depthAttachment.createView();
-
-  // Create a valid pass for control.
-  const validEncoder = t.device.createCommandEncoder();
-  const validPass = validEncoder.beginRenderPass({
-    colorAttachments: [],
-    depthStencilAttachment: {
-      attachment: depthAttachmentView,
-      depthLoadValue: 'load',
-      depthStoreOp: 'clear',
-      depthReadOnly: false,
-      stencilLoadValue: 'load',
-      stencilStoreOp: 'clear',
-      stencilReadOnly: false,
-    },
-  });
-  validPass.endPass();
-
-  // A valid pass should succeed.
-  t.device.defaultQueue.submit([validEncoder.finish()]);
-
-  // Create an invalid pass with depthReadOnly=true and depthStoreOp='clear'.
-  const invalidEncoder = t.device.createCommandEncoder();
-  const invalidPass = invalidEncoder.beginRenderPass({
-    colorAttachments: [],
-    depthStencilAttachment: {
-      attachment: depthAttachmentView,
-      depthLoadValue: 'load',
-      depthStoreOp: 'clear',
-      depthReadOnly: true,
-      stencilLoadValue: 'load',
-      stencilStoreOp: 'clear',
-      stencilReadOnly: false,
-    },
-  });
-  invalidPass.endPass();
-
-  // Using depthReadOnly=true and depthStoreOp='clear' should cause a validation error.
-  t.expectValidationError(() => {
-    t.device.defaultQueue.submit([invalidEncoder.finish()]);
-  });
-});
-
-g.test('stencilStoreOp_must_be_store_when_stencilReadOnly_is_true').fn(async t => {
-  const depthAttachment = t.device.createTexture({
-    format: 'depth24plus-stencil8',
-    size: { width: 1, height: 1, depth: 1 },
-    usage: GPUTextureUsage.COPY_SRC | GPUTextureUsage.OUTPUT_ATTACHMENT,
-  });
-  const depthAttachmentView = depthAttachment.createView();
-
-  // Create valid pass for control.
-  const validEncoder = t.device.createCommandEncoder();
-  const validPass = validEncoder.beginRenderPass({
-    colorAttachments: [],
-    depthStencilAttachment: {
-      attachment: depthAttachmentView,
-      depthLoadValue: 'load',
-      depthStoreOp: 'clear',
-      depthReadOnly: false,
-      stencilLoadValue: 'load',
-      stencilStoreOp: 'clear',
-      stencilReadOnly: false,
-    },
-  });
-  validPass.endPass();
-
-  // A valid pass should succeed.
-  t.device.defaultQueue.submit([validEncoder.finish()]);
-
-  // Create an invalid pass with stencilReadOnly=true and stencilStoreOp='clear'.
-  const invalidEncoder = t.device.createCommandEncoder();
-  const invalidPass = invalidEncoder.beginRenderPass({
-    colorAttachments: [],
-    depthStencilAttachment: {
-      attachment: depthAttachmentView,
-      depthLoadValue: 'load',
-      depthStoreOp: 'clear',
-      depthReadOnly: false,
-      stencilLoadValue: 'load',
-      stencilStoreOp: 'clear',
-      stencilReadOnly: true,
-    },
-  });
-  invalidPass.endPass();
-
-  // Using stencilReadOnly=true and stencilStoreOp='clear' should cause a validation error.
-  t.expectValidationError(() => {
-    t.device.defaultQueue.submit([invalidEncoder.finish()]);
-  });
-});
-
-g.test('depthReadOnly_must_be_the_same_as_stencilReadOnly').fn(async t => {
-  const depthAttachment = t.device.createTexture({
-    format: 'depth24plus-stencil8',
-    size: { width: 1, height: 1, depth: 1 },
-    usage: GPUTextureUsage.COPY_SRC | GPUTextureUsage.OUTPUT_ATTACHMENT,
-  });
-  const depthAttachmentView = depthAttachment.createView();
-
-  // Create valid read-only pass for control.
-  const validEncoder = t.device.createCommandEncoder();
-  const validPass = validEncoder.beginRenderPass({
-    colorAttachments: [],
-    depthStencilAttachment: {
-      attachment: depthAttachmentView,
-      depthLoadValue: 'load',
-      depthStoreOp: 'store',
-      depthReadOnly: false,
-      stencilLoadValue: 'load',
-      stencilStoreOp: 'store',
-      stencilReadOnly: false,
-    },
-  });
-  validPass.endPass();
-
-  // Valid pass should succeed.
-  t.device.defaultQueue.submit([validEncoder.finish()]);
-
-  // Create an invalid pass that is read-only, but with mismatched depthReadOnly and stencilReadOnly values.
-  const invalidEncoder = t.device.createCommandEncoder();
-  const invalidPass = invalidEncoder.beginRenderPass({
-    colorAttachments: [],
-    depthStencilAttachment: {
-      attachment: depthAttachmentView,
-      depthLoadValue: 'load',
-      depthStoreOp: 'store',
-      depthReadOnly: false,
-      stencilLoadValue: 'load',
-      stencilStoreOp: 'store',
-      stencilReadOnly: true,
-    },
-  });
-  invalidPass.endPass();
-
-  // Mismatched depthReadOnly and stencilReadOnly values should cause a validation error.
-  t.expectValidationError(() => {
-    t.device.defaultQueue.submit([invalidEncoder.finish()]);
-  });
-});
